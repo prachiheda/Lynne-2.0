0ce4d73bef2be3e336a9c30759769373d5227330
Ella Chung <echung873@gmail.com>
2025-03-25 23:16:36 -0700

    init settings view

--- Lynne 2.0/Features/Settings/Views/SettingsView.swift
+++ Lynne 2.0/Features/Settings/Views/SettingsView.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("lynne-logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 250, height: 250)
+            
+            Text("Reminder Settings")
+                .font(.title)
+                .font(.system(size: 72))
+
+            RoundedRectangle(cornerRadius: 30)
+                .stroke(Color(hex: "#46403A"), lineWidth: 4)
+                .frame(width: 300, height: 200)
+                .overlay(
+                    HStack {
+                        VStack {
+                            Text("Set Daily Reminder")
+                            Text("Remind Every")
+                            
+                        }
+                        VStack {
+                            Text("07:00 AM")
+                            Text("07:00 AM")
+                        }
+                        
+                    }
+
+                )
+            
+            
+            Text("Taking your pill at the same time every day increases effectiveness! Taking it at least within x minutes of your daily reminder is recommended.")
+                .padding(20)
+            
+        }
     }
 }
 
